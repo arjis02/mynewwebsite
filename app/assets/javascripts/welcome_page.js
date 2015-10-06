f05aaa86592cde14c7fab5f363e5f4979f0e2464
arjis02 <rj.ramos@gmail.com>
2015-10-06 19:53:03 -0400

    Update CSS for welcome page

--- app/assets/javascripts/welcome_page.js
+++ app/assets/javascripts/welcome_page.js
@@ -2,9 +2,10 @@ $(document).on('ready page:load', function(){
 	$(function(){ $(document).foundation(); });
 	$('.aboutme-link').on('click', function(event){
 		event.preventDefault();
-		$(this).unbind('click');
+		$(this).off('click');
 		$.get('/aboutme').done(function(content){
 			$('.row').after(content);
+			$('.aboutme').fadeIn();
 		}).fail(function(){
 			console.log('bad things');
 		});
