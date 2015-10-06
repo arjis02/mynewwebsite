1fe29d900236fc9016043318fb0d44de57fb92b6
arjis02 <rj.ramos@gmail.com>
2015-10-06 18:18:00 -0400

    Add route for aboutme

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
     get 'blogs' => 'blogs#index'
+    get 'aboutme' => 'aboutme#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
